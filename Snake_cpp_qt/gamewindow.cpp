b73ad0490b738a1a153b3918702aa4059e8a9c7b
CRaNkXD <crank.xd@outlook.com>
2020-09-08 21:45:46 +0200

    added function add_points added function display_points added function start_game added QTimer to generate game loop added function reset_game added function display_game_state added function update_game_state override function keyPressEvent updated constructor

--- Snake_cpp_qt/gamewindow.cpp
+++ Snake_cpp_qt/gamewindow.cpp
@@ -1,11 +1,52 @@
+#include <iostream>
+#include <algorithm>
+
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
+#include "model/snake.h"
+#include "model/food.h"
+#include "helper/helper.h"
+#include "constants/constants.h"
 
-GameWindow::GameWindow(QWidget *parent)
+GameWindow::GameWindow(QWidget *parent, const UShort &size_x, const UShort &size_y)
     : QMainWindow(parent)
     , ui(new Ui::GameWindow)
+    , m_size_x(size_x)
+    , m_size_y(size_y)
+    , m_playing_field(m_size_x, std::vector<QLabel *>(this->m_size_y))
+    , m_start_position(this->m_size_x / 2, this->m_size_y / 2)
+    , m_snake(this->m_start_position, constants::Direction::RIGHT,
+              4, this->m_size_x * this->m_size_y + 1)
+    , m_in_game(false)
+    , m_game_updated(true)
+    , m_won(false)
+    , m_points(0)
 {
     ui->setupUi(this);
+    this->m_game_timer = new QTimer(this);
+    this->m_exit_menu = ui->actionExit_3;
+    this->m_start_button = ui->btn_start;
+    this->m_points_label = ui->label_points;
+    this->display_points();
+
+    // init food with eaten so that the first food will be generated
+    this->m_food.eaten = true;
+
+    for (UShort i = 0; i < this->m_size_x; ++i)
+    {
+        for (UShort j = 0; j < this->m_size_y; ++j)
+        {
+            QString field_name = "field_" + QString::number(i) + "_" +  QString::number(j);
+            this->m_playing_field[i][j] = GameWindow::findChild<QLabel *>(field_name);
+            this->m_playing_field[i][j]->setScaledContents(true);
+            this->m_playing_field[i][j]->setPixmap(QPixmap(this->m_empty_field_icon_path));
+        }
+    }
+
+    // Connect slots
+    connect(m_game_timer, SIGNAL(timeout()), this, SLOT(update_game_state()));
+    connect(m_start_button, SIGNAL(clicked()), this, SLOT(start_game()));
+    connect(m_exit_menu, SIGNAL (triggered()), this, SLOT (close()));
 }
 
 GameWindow::~GameWindow()
@@ -13,3 +54,301 @@ GameWindow::~GameWindow()
     delete ui;
 }
 
+void GameWindow::add_points(const int &points)
+{
+    this->m_points += points;
+}
+
+void GameWindow::display_points()
+{
+    QString points = QString::number(this->m_points);
+    this->m_points_label->setText(points);
+}
+
+void GameWindow::start_game()
+{
+    this->m_in_game = true;
+    this->m_game_timer->start(150);
+}
+
+void GameWindow::reset_game()
+{
+    this->m_in_game = false;
+    this->m_game_updated = false;
+    this->m_food.eaten = true;
+    this->m_game_timer->stop();
+    for (UShort i = 0; i < this->m_size_x; ++i)
+    {
+        for (UShort j = 0; j < this->m_size_y; ++j)
+        {
+            this->m_playing_field[i][j]->setPixmap(QPixmap(this->m_empty_field_icon_path));
+        }
+    }
+    this->m_snake = Snake(this->m_start_position, constants::Direction::RIGHT, 4, this->m_size_x * this->m_size_y + 1);
+    this->m_points = 0;
+    this->display_points();
+}
+
+void GameWindow::display_game_state()
+{
+    SnakeVec snake_vec = this->m_snake.get_snake();
+    for (auto it_snake = snake_vec.begin();
+         it_snake != snake_vec.begin() + this->m_snake.get_length();
+         ++it_snake)
+    {
+        if (it_snake == snake_vec.begin())
+        {
+            switch (it_snake->direction)
+            {
+                case constants::Direction::UP:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_head_UP_icon_path));
+                    break;
+                }
+                case constants::Direction::DOWN:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_head_DOWN_icon_path));
+                    break;
+                }
+                case constants::Direction::LEFT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_head_LEFT_icon_path));
+                    break;
+                }
+                case constants::Direction::RIGHT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_head_RIGHT_icon_path));
+                    break;
+                }
+            }
+        }
+        else if (it_snake == snake_vec.begin() + this->m_snake.get_length() - 2)
+        {
+            switch (it_snake->direction)
+            {
+                case constants::Direction::UP:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_tail_UP_icon_path));
+                    break;
+                }
+                case constants::Direction::DOWN:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_tail_DOWN_icon_path));
+                    break;
+                }
+                case constants::Direction::LEFT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_tail_LEFT_icon_path));
+                    break;
+                }
+                case constants::Direction::RIGHT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_tail_RIGHT_icon_path));
+                    break;
+                }
+            }
+        }
+        // the last part is used to clean up the path behind the snake
+        else if (it_snake == snake_vec.begin() + this->m_snake.get_length() - 1)
+        {
+            switch (it_snake->direction)
+            {
+                case constants::Direction::UP:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_empty_field_icon_path));
+                    break;
+                }
+                case constants::Direction::DOWN:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_empty_field_icon_path));
+                    break;
+                }
+                case constants::Direction::LEFT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_empty_field_icon_path));
+                    break;
+                }
+                case constants::Direction::RIGHT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_empty_field_icon_path));
+                    break;
+                }
+            }
+        }
+        else if (it_snake->direction != std::next(it_snake)->direction)
+        {
+            // TODO: consider renaming the curve pics
+            if (it_snake->direction == constants::Direction::UP)
+            {
+                if (std::next(it_snake)->direction == constants::Direction::RIGHT)
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_UP_LEFT_icon_path));
+                }
+                else
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_UP_RIGHT_icon_path));
+                }
+            }
+            else if (it_snake->direction == constants::Direction::DOWN)
+            {
+                if (std::next(it_snake)->direction == constants::Direction::RIGHT)
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_DOWN_LEFT_icon_path));
+                }
+                else
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_DOWN_RIGHT_icon_path));
+                }
+            }
+            else if (it_snake->direction == constants::Direction::LEFT)
+            {
+                if (std::next(it_snake)->direction == constants::Direction::UP)
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_DOWN_LEFT_icon_path));
+                }
+                else
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_UP_LEFT_icon_path));
+                }
+            }
+            else if (it_snake->direction == constants::Direction::RIGHT)
+            {
+                if (std::next(it_snake)->direction == constants::Direction::UP)
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_DOWN_RIGHT_icon_path));
+                }
+                else
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_curve_UP_RIGHT_icon_path));
+                }
+            }
+        }
+        else if (it_snake->direction == std::next(it_snake)->direction)
+        {
+            switch (it_snake->direction)
+            {
+                case constants::Direction::UP:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_vertical_icon_path));
+                    break;
+                }
+                case constants::Direction::DOWN:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_vertical_icon_path));
+                    break;
+                }
+                case constants::Direction::LEFT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_horizontal_icon_path));
+                    break;
+                }
+                case constants::Direction::RIGHT:
+                {
+                    this->m_playing_field[it_snake->position.first][it_snake->position.second]->setPixmap(QPixmap(this->m_snake_body_horizontal_icon_path));
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void GameWindow::update_game_state()
+{
+    this->m_snake.move();
+    if (snake_hit_snake(this->m_snake) ||
+        snake_hit_wall(this->m_snake, this->m_size_x, this->m_size_y))
+    {
+        this->reset_game();
+        // TODO: show highscore list if and prompt input if a new highscore was set
+    }
+    else
+    {
+        if (snake_ate_food(this->m_snake, this->m_food))
+        {
+            this->m_snake.add_part();
+            this->add_points(this->m_food.points);
+            this->display_points();
+            this->m_food.eaten = true;
+            if (this->m_snake.get_length() == this->m_size_x * this->m_size_y)
+            {
+                this->m_won = true;
+            }
+        }
+
+        if (!this->m_won)
+        {
+            if (this->m_food.eaten)
+            {
+                do{
+                    this->m_food.position = rand_position(this->m_size_x - 1, this->m_size_y - 1);
+                } while(this->m_snake.is_occupied(this->m_food.position));
+                this->m_food.eaten = false;
+            }
+            this->m_playing_field[this->m_food.position.first][this->m_food.position.second]->setPixmap(QPixmap(this->m_food_icon_path));
+
+            this->display_game_state();
+            this->m_game_updated = true;
+        }
+        else
+        {
+            this->reset_game();
+            // TODO: show you have won message
+        }
+
+    }
+}
+
+void GameWindow::show_highscore()
+{
+
+}
+
+void GameWindow::new_game()
+{
+
+}
+
+void GameWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (this->m_in_game && this->m_game_updated)
+    {
+        constants::Direction current_direction = this->m_snake.get_front_direction();
+        switch(event->key())
+        {
+            case Qt::Key_W:
+            {
+                if (current_direction != constants::Direction::DOWN)
+                    this->m_snake.set_front_direction(constants::Direction::UP);
+                break;
+            }
+            case Qt::Key_A:
+            {
+                if (current_direction != constants::Direction::RIGHT)
+                    this->m_snake.set_front_direction(constants::Direction::LEFT);
+                break;
+            }
+            case Qt::Key_S:
+            {
+                if (current_direction != constants::Direction::UP)
+                    this->m_snake.set_front_direction(constants::Direction::DOWN);
+                break;
+            }
+            case Qt::Key_D:
+            {
+                if (current_direction != constants::Direction::LEFT)
+                    this->m_snake.set_front_direction(constants::Direction::RIGHT);
+                break;
+            }
+            default:
+            {
+                return;
+            }
+        }
+        this->m_game_updated = false;
+    }
+}
+
+void GameWindow::show_settings()
+{
+
+}
--- Snake_cpp_qt/gamewindow.h
+++ Snake_cpp_qt/gamewindow.h
@@ -1,20 +1,72 @@
 #pragma once
-
+#include <QLabel>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTimer>
+#include <QKeyEvent>
+
+#include <vector>
+
+#include "model/snake.h"
+#include "model/food.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class GameWindow; }
 QT_END_NAMESPACE
+
+typedef std::vector<std::vector<QLabel *>> PlayingField;
 
 class GameWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    GameWindow(QWidget *parent = nullptr);
+    GameWindow(QWidget *parent = nullptr, const UShort &size_x = 20, const UShort &size_y = 20);
     ~GameWindow();
+    void add_points(const int &points);
+    void display_points();
+    void reset_game();
+    void display_game_state();
 
 private:
     Ui::GameWindow *ui;
+    QAction *m_exit_menu;
+    QPushButton *m_start_button;
+    QLabel *m_points_label;
+    UShort m_size_x;
+    UShort m_size_y;
+    PlayingField m_playing_field;
+    Position m_start_position;
+    Snake m_snake;
+    Food m_food;
+    bool m_in_game;
+    bool m_game_updated;
+    bool m_won;
+    int m_points;
+    QTimer *m_game_timer;
+    QString m_snake_body_horizontal_icon_path = ":/pics/snake/snake_body_horizontal.png";
+    QString m_snake_body_vertical_icon_path = ":/pics/snake/snake_body_vertical.png";
+    QString m_snake_head_UP_icon_path = ":/pics/snake/snake_head_UP.png";
+    QString m_snake_head_DOWN_icon_path = ":/pics/snake/snake_head_DOWN.png";
+    QString m_snake_head_LEFT_icon_path = ":/pics/snake/snake_head_LEFT.png";
+    QString m_snake_head_RIGHT_icon_path = ":/pics/snake/snake_head_RIGHT.png";
+    QString m_snake_tail_UP_icon_path = ":/pics/snake/snake_tail_UP.png";
+    QString m_snake_tail_DOWN_icon_path = ":/pics/snake/snake_tail_DOWN.png";
+    QString m_snake_tail_LEFT_icon_path = ":/pics/snake/snake_tail_LEFT.png";
+    QString m_snake_tail_RIGHT_icon_path = ":/pics/snake/snake_tail_RIGHT.png";
+    QString m_snake_body_curve_DOWN_LEFT_icon_path = ":/pics/snake/snake_body_curve_DOWN_LEFT.png";
+    QString m_snake_body_curve_DOWN_RIGHT_icon_path = ":/pics/snake/snake_body_curve_DOWN_RIGHT.png";
+    QString m_snake_body_curve_UP_LEFT_icon_path = ":/pics/snake/snake_body_curve_UP_LEFT.png";
+    QString m_snake_body_curve_UP_RIGHT_icon_path = ":/pics/snake/snake_body_curve_UP_RIGHT.png";
+    QString m_empty_field_icon_path = ":/pics/snake/empty_field.png";
+    QString m_food_icon_path = ":/pics/snake/food.png";
+
+private slots:
+    void start_game();
+    void show_highscore();
+    void new_game();
+    void keyPressEvent(QKeyEvent *event) override;
+    void show_settings();
+    void update_game_state();
 
 };
