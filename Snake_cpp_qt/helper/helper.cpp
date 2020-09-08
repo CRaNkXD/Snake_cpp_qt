01a15ba6ebb62d2b18e65fc6af69d51c9eae999f
CRaNkXD <crank.xd@outlook.com>
2020-09-08 21:39:37 +0200

    added snake_hit_wall function added snake_hit_snake function added snake_ate_food function updated return type rand_position

--- Snake_cpp_qt/helper/helper.cpp
+++ Snake_cpp_qt/helper/helper.cpp
@@ -1,13 +1,46 @@
 #include "helper.h"
 
+#include <limits>
 #include <random>
+#include <cmath>
+#include <algorithm>
 
-Position* rand_position(const UShort &max_x, const UShort &max_y)
+Position rand_position(const UShort &max_x, const UShort &max_y)
 {
+    UShort max = std::max(max_x, max_y);
     std::random_device seed_gen;
     std::mt19937 gen(seed_gen());
-    std::uniform_int_distribution<UShort> dist(max_x, max_y);
+    std::uniform_int_distribution<UShort> dist(0, max);
     Position rand_position{ dist(gen), dist(gen) };
-    Position *position = &rand_position ;
-    return position;
+    return rand_position;
+}
+
+bool snake_hit_wall(const Snake &snake, const UShort &max_x, const UShort &max_y)
+{
+    SnakeVec snake_vec = snake.get_snake();
+
+    if (snake_vec.front().position.first == max_x ||
+        snake_vec.front().position.second == max_y ||
+        snake_vec.front().position.first == std::numeric_limits<UShort>::max() ||
+        snake_vec.front().position.second == std::numeric_limits<UShort>::max())
+    {
+        return true;
+    }
+    return false;
+}
+
+bool snake_hit_snake(const Snake &snake)
+{
+    SnakeVec snake_vec = snake.get_snake();
+
+    return snake_vec.begin() + snake.get_length() != std::find_if(snake_vec.begin() + 1, snake_vec.begin() + snake.get_length(),
+        [&](const SnakePart& snake_part){
+        return snake_part.position == snake_vec.front().position;
+    });
+}
+
+bool snake_ate_food(const Snake &snake, const Food &food)
+{
+    SnakeVec snake_vec = snake.get_snake();
+    return snake_vec.front().position == food.position;
 }
--- Snake_cpp_qt/helper/helper.h
+++ Snake_cpp_qt/helper/helper.h
@@ -1,5 +1,9 @@
 #pragma once
 
 #include "model/snake.h"
+#include "model/food.h"
 
-Position* rand_position(const UShort &max_x, const UShort &max_y);
+Position rand_position(const UShort &max_x, const UShort &max_y);
+[[nodiscard]]bool snake_hit_wall(const Snake &snake, const UShort &max_x, const UShort &max_y);
+[[nodiscard]]bool snake_hit_snake(const Snake &snake);
+[[nodiscard]]bool snake_ate_food(const Snake &snake, const Food &food);
