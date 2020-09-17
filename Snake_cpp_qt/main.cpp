522e0da9c02bf502f82473de45bef1c329d106a3
CRaNkXD <crank.xd@outlook.com>
2020-09-18 00:07:04 +0200

    init the highscore list at start if the file does not already exist

--- Snake_cpp_qt/main.cpp
+++ Snake_cpp_qt/main.cpp
@@ -1,9 +1,14 @@
 #include "gamewindow.h"
+#include "helper/helper.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    // init highscore list
+    std::vector<std::pair<std::string, std::string>> highscore_list;
+    save_highscore_list(highscore_list);
+
     QApplication a(argc, argv);
     GameWindow w;
     w.show();
