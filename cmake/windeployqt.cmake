47d13247a83922cca031ddf49814ada7d8cf7e55
CRaNkXD <crank.xd@outlook.com>
2025-09-04 14:34:20 +0200

    update to qt 6; comment out deployment for windows

--- cmake/windeployqt.cmake
+++ cmake/windeployqt.cmake
@@ -2,7 +2,7 @@ find_package(Qt5Core REQUIRED)
 
 # get absolute path to qmake, then use it to find windeployqt executable
 
-get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
 get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
 
 function(windeployqt target)
